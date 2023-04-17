f3030c848be84d6697d2baab5d17d8d96ce66dec
minstrike520 <minstrike520@gmail.com>
2023-04-18 00:07:29 +0800

    Draws images

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ var config = {
       update: update
     }
   };
-  
+
   var game = new Phaser.Game(config);
 
-  function preload() {}
+  function preload() {
+    this.load.image('tile1', 'assets/tile1.png');
+    this.load.image('tile2', 'assets/tile2.png');
+    this.load.image('tile3', 'assets/tile3.png')
+  }
   
-  function create() {}
+  function create() {
+    this.add.image(0, 0, 'tile1').setOrigin(0, 0);
+    this.
+    this.add.image(50, 0, 'tile2').setOrigin(0, 0);
+    this.add.image(0, 50, 'tile3').setOrigin(0, 0);
+    // 
+  }
   
   function update() {}
